3b211f543b092c9e4ede9a58e8ee42f4d9565b14
Dr00pyd00 <kottakcode@gmail.com>
2026-09-26 16:05:50 +0400

    feat(core) database.py add comments for package

--- app/core/database.py
+++ app/core/database.py
@@ -4,6 +4,11 @@ from sqlalchemy.orm import DeclarativeBase
 
 from app.core.settings import settings 
 
+"""
+    ( uv or pip)
+    NEED: to install -> asyncpg    ( for the app )
+                     -> psycopg[binary]   ( for alembic )
+"""
 
 engine = create_async_engine(url=settings.get_db_url)
 
